ed9ec6f154c81bad80ebde1aa415bd702edda15e
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-10-31 11:29:51 +0200

    Create app

---
+++ react-demo-components/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+const movies = [
+  {
+    title: 'The Matrix',
+    description: 'Description',
+  },
+  {
+    title: 'Harry Potter',
+    description: 'Description',
+  },
+  {
+    title: 'Lord of the Rings',
+    description: 'Description',
+  },
+];
+
+function App() {
+
+  return (
+    <div>
+      <h1>My First Dynamic React App</h1>
+    </div>
+  );
+}
+
+export default App;
