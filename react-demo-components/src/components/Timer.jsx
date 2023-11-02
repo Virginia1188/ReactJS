d9d246d96f3cc800592b947c3d4abff5865cc27b
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-02 10:01:42 +0200

    add useEffect for timer

--- react-demo-components/src/components/Timer.jsx
+++ react-demo-components/src/components/Timer.jsx
@@ -1,14 +1,14 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Timer(props) {
 
     const [time, setTime] = useState(props.startTime);
 
-    setTimeout(() => {
-        setTime(time + 1);
-       
-    }, 1000);
+    useEffect(() => {
+        setTimeout(() => setTime(t => t + 1), 1000);
+    }, [time])
+
 
     return (
         <div>
