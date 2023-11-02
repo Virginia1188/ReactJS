065e00bfe51fa2d47b2da51db76595c97ce46d50
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-02 15:13:08 +0200

    Create Footer component

---
+++ todo-list-app/client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Footer() {
+
+    return (
+        <div>
+            <footer className="footer">
+                <p>Copyright © designed by Mihail Valkov</p>
+            </footer>
+        </div>
+    )
+}
