9297bfbd4de4ede340fe81373f00f32a0e42869b
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-03 14:29:06 +0200

    Create UserListTable

--- workshop-components/client/src/App.jsx
+++ workshop-components/client/src/App.jsx
@@ -3,16 +3,20 @@ import './styles.css'
 
 import Header from './components/Header'
 import Footer from './components/Foother'
+import UserList from './components/UserList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Working</h1>
-
       <Header />
 
+      <main className="main">
+      <UserList />
+
+
+      </main>
 
       <Footer />
     </div>
