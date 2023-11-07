0fdb129a59e468739b42f442940c364f793e9693
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-07 14:01:29 +0200

    Add basic html for navbar and topbar

---
+++ Project/client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Topbar from './components/Topbar';
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <div>
+      <Topbar />
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
