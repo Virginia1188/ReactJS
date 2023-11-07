6e5a3987d3bf069cafdf3755343fcc9f772ff617
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-07 15:13:20 +0200

    Update html for topbar and navbar

--- Project/client/src/components/Navbar.jsx
+++ Project/client/src/components/Navbar.jsx
@@ -5,24 +5,24 @@ export default function Navbar() {
             <div className="container-lg position-relative p-0 px-lg-3" style={{zIndex: 9}}>
                 <nav className="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
                     <a href="index.html" className="navbar-brand d-block d-lg-none">
-                        <h1 className="m-0 display-4 text-primary"><span className="text-secondary">i</span>CREAM</h1>
+                        <h1 className="m-0 display-4 text-primary"><span className="text-secondary"></span>Еремия</h1>
                     </a>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                         <div className="navbar-nav ml-auto py-0">
-                            <a href="index.html" className="nav-item nav-link active">Home</a>
-                            <a href="about.html" className="nav-item nav-link">About</a>
-                            <a href="product.html" className="nav-item nav-link">Product</a>
+                            <a href="index.html" className="nav-item nav-link active">Зали</a>
+                            <a href="about.html" className="nav-item nav-link">График</a>
+                            <a href="product.html" className="nav-item nav-link">За Нас</a>
                         </div>
                         <a href="index.html" className="navbar-brand mx-5 d-none d-lg-block">
-                            <h1 className="m-0 display-4 text-primary"><span className="text-secondary">i</span>CREAM</h1>
+                            <h1 className="m-0 display-4 text-primary"><span className="text-secondary"></span>Еремия</h1>
                         </a>
                         <div className="navbar-nav mr-auto py-0">
-                            <a href="service.html" className="nav-item nav-link">Service</a>
-                            <a href="gallery.html" className="nav-item nav-link">Gallery</a>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
+                            <a href="service.html" className="nav-item nav-link">Услуги</a>
+                            <a href="gallery.html" className="nav-item nav-link">Събития</a>
+                            <a href="contact.html" className="nav-item nav-link">Контакти</a>
                         </div>
                     </div>
                 </nav>
--- Project/client/src/components/Topbar.jsx
+++ Project/client/src/components/Topbar.jsx
@@ -6,11 +6,11 @@ export default function Topbar() {
                     <div className="row">
                         <div className="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
                             <div className="d-inline-flex align-items-center">
-                                <a className="text-white pr-3" href="">FAQs</a>
+                                <a className="text-white pr-3" href="">Моята Зала</a>
                                 <span className="text-white">|</span>
-                                <a className="text-white px-3" href="">Help</a>
+                                <a className="text-white px-3" href="">Любими</a>
                                 <span className="text-white">|</span>
-                                <a className="text-white pl-3" href="">Support</a>
+                                <a className="text-white pl-3" href="">Профил</a>
                             </div>
                         </div>
                         <div className="col-md-6 text-center text-lg-right">
