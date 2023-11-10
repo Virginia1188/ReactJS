5b8ec1d7a9ceebeca23ba1fe5bf92a37ff939961
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-10 12:26:11 +0200

    Update the navigartion with the new routes

--- routing-demo/src/App.jsx
+++ routing-demo/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'; 
 import Navigation from './components/Navigation'
+import CharactersList from './components/CharactersList'
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <>
       <Navigation/>
-      <h1>React Router</h1>
+      <h5>React Router with react bootstrap</h5>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/characters' element={<CharactersList/>} />
+      </Routes>
+
+      <footer>All rights reserved &copy;</footer>
     </>
   )
 }
--- routing-demo/src/components/Navigation.jsx
+++ routing-demo/src/components/Navigation.jsx
@@ -9,13 +9,14 @@ export default function Navigation() {
     return(
         <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Router</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">React-Router</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
+              <Nav.Link as={Link} to="/characters">Characters</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
