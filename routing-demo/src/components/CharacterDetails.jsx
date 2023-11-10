cdddb8a3e73e3b37f858593516a2ceece34eb5e4
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-10 13:41:33 +0200

    Add not found for characters

--- routing-demo/src/components/CharacterDetails.jsx
+++ routing-demo/src/components/CharacterDetails.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function CharacterDetails() {
-    // const name = 'some'
-    const { id } = useParams()
+    
+    const { id } = useParams();
     const [character, setCharacter] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`https://swapi.dev/api/people/${id}`)
-            .then(res => res.json())
+            .then(res => {
+                if(!res.ok){
+                    throw new Error('Not found')
+                }
+            })
             .then(setCharacter)
+            .catch((err) => {
+                navigate('/characters');
+            })
     }, [id])
     return (
         <>
