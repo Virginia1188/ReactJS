6bfe5e9a85a6141e5632aa083e09c7619c174bc6
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-10 12:24:01 +0200

    Create about component

---
+++ routing-demo/src/components/About.jsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+    return(
+        <div>
+            <h2>About Page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, odit! Odio facere recusandae odit eius rerum possimus voluptates mollitia, accusamus minima nam doloribus atque beatae error corporis fuga nesciunt porro sequi quaerat deleniti voluptas a numquam consequuntur vel? Illum assumenda id nesciunt provident totam voluptatibus ipsa laboriosam quod, blanditiis debitis!</p>
+        </div>
+    );
+}
