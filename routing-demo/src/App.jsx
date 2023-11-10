dd20a874def647b89912d441090fadd615d56d0c
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-10 13:41:04 +0200

    Create not found component

--- routing-demo/src/App.jsx
+++ routing-demo/src/App.jsx
@@ -1,22 +1,24 @@
-import { Route, Routes } from 'react-router-dom'; 
+import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation'
 import CharactersList from './components/CharactersList'
 import About from './components/About';
 import Home from './components/Home';
 import CharacterDetails from './components/CharacterDetails';
+import NotFound from './components/NotFound';
 
 function App() {
 
   return (
     <>
-      <Navigation/>
+      <Navigation />
       <h5>React Router with react bootstrap</h5>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/characters' element={<CharactersList/>} />
+        <Route path='/characters' element={<CharactersList />} />
         <Route path='/characters/:id' element={<CharacterDetails />} />
+        <Route path='*' element={<NotFound />}/>
       </Routes>
 
       <footer>All rights reserved &copy;</footer>
