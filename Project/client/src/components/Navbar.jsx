9a9ab050e74f6e75a744bdbdc2ca34caed352557
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-13 12:51:34 +0200

    create styles for navbar

--- Project/client/src/components/Navbar.jsx
+++ Project/client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
     return (
@@ -5,7 +6,7 @@ export default function Navbar() {
             <div className="container-lg position-relative p-0 px-lg-3" style={{zIndex: 9}}>
                 <nav className="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
                     <a href="index.html" className="navbar-brand d-block d-lg-none">
-                        <h1 className="m-0 display-4 text-primary"><span className="text-secondary"></span>Еремия</h1>
+                        <h1 className="m-0 display-4 text-primary" ><span className="text-secondary"></span><div className={styles.navTitle}>Еремия</div></h1>
                     </a>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span className="navbar-toggler-icon"></span>
@@ -17,7 +18,7 @@ export default function Navbar() {
                             <a href="product.html" className="nav-item nav-link">За Нас</a>
                         </div>
                         <a href="index.html" className="navbar-brand mx-5 d-none d-lg-block">
-                            <h1 className="m-0 display-4 text-primary"><span className="text-secondary"></span>Еремия</h1>
+                            <h1 className="m-0 display-4 text-primary " ><span className="text-secondary"></span><div className={styles.navTitle}>Еремия</div></h1>
                         </a>
                         <div className="navbar-nav mr-auto py-0">
                             <a href="service.html" className="nav-item nav-link">Услуги</a>
