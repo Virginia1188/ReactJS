62efbdbb5efba44d26f3537908c365438cf6ab10
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-13 13:31:19 +0200

    Add react router

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,7 +1,9 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Topbar from './components/Topbar';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
 
@@ -9,6 +11,15 @@ function App() {
     <div>
       <Topbar />
       <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/' element={Home} /> */}
+        {/* <Route path='/' element={Home} /> */}
+        {/* <Route path='/' element={Home} /> */}
+        {/* <Route path='/' element={Home} /> */}
+      </Routes>
     </div>
   )
 }
--- Project/client/src/components/Navbar.jsx
+++ Project/client/src/components/Navbar.jsx
@@ -1,29 +1,30 @@
 import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <div className="container-fluid position-relative nav-bar p-0">
             <div className="container-lg position-relative p-0 px-lg-3" style={{zIndex: 9}}>
                 <nav className="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
-                    <a href="index.html" className="navbar-brand d-block d-lg-none">
+                    <Link to='/' className="navbar-brand d-block d-lg-none">
                         <h1 className="m-0 display-4 text-primary" ><span className="text-secondary"></span><div className={styles.navTitle}>Еремия</div></h1>
-                    </a>
+                    </Link>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                         <div className="navbar-nav ml-auto py-0">
-                            <a href="index.html" className="nav-item nav-link active">Зали</a>
-                            <a href="about.html" className="nav-item nav-link">График</a>
-                            <a href="product.html" className="nav-item nav-link">За Нас</a>
+                            <Link to="/studios" className="nav-item nav-link active">Зали</Link>
+                            <Link to="/shedule" className="nav-item nav-link">График</Link>
+                            <Link to="/about" className="nav-item nav-link">За Нас</Link>
                         </div>
-                        <a href="index.html" className="navbar-brand mx-5 d-none d-lg-block">
+                        <Link to="/" className="navbar-brand mx-5 d-none d-lg-block">
                             <h1 className="m-0 display-4 text-primary " ><span className="text-secondary"></span><div className={styles.navTitle}>Еремия</div></h1>
-                        </a>
+                        </Link>
                         <div className="navbar-nav mr-auto py-0">
-                            <a href="service.html" className="nav-item nav-link">Услуги</a>
-                            <a href="gallery.html" className="nav-item nav-link">Събития</a>
-                            <a href="contact.html" className="nav-item nav-link">Контакти</a>
+                            <Link to="/services" className="nav-item nav-link">Услуги</Link>
+                            <Link to="/events" className="nav-item nav-link">Събития</Link>
+                            <Link to="/contacts" className="nav-item nav-link">Контакти</Link>
                         </div>
                     </div>
                 </nav>
