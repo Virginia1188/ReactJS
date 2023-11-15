c56886de16c2d3f3f17db4227c71f9d59818d7d3
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-11-15 13:38:45 +0200

    Create game list item

--- Game Play/client/src/components/Game-List/GameList.jsx
+++ Game Play/client/src/components/Game-List/GameList.jsx
@@ -1,48 +1,27 @@
 import { useEffect, useState } from "react";
 import * as gameService from '../../services/gameService';
+import GameListItem from "./Game-List-Item/GameListItem";
 
 export default function GameList() {
-    const [games,setGames] = useState([]);
+    const [games, setGames] = useState([]);
 
     useEffect(() => {
         gameService.getAll()
             .then(result => setGames(result));
 
     }, []);
-    console.log(games);
 
     return (
         < section id="catalog-page" >
             <h1>All Games</h1>
-            {/* <!--Display div: with information about every game(if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
+            {games.map(game => (
+                <GameListItem key={game._id} {...game} />
+            ))}
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+            {games.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
 
-            {/* <!--Display paragraph: If there is no games-- > */}
-            <h3 className="no-articles">No articles yet</h3>
         </section >);
 }
